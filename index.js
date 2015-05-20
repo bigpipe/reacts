c63dc60256bfb119a6cd4a67c691ad04ad3da8d7
Swaagie <info@martijnswaagman.nl>
2015-05-20 10:04:30 -0700

    [minor] add name, comments and templates

--- index.js
+++ index.js
@@ -4,6 +4,13 @@ var Fittings = require('fittings')
   , path = require('path')
   , fs = require('fs');
 
+/**
+ * Read files out of our instructions directory.
+ *
+ * @param {String} file Filename that we should read.
+ * @returns {String}
+ * @api private
+ */
 function read(file) {
   return fs.readFileSync(path.join(__dirname, 'fittings', file), 'utf-8');
 }
@@ -12,7 +19,12 @@ function read(file) {
 // Expose the React fittings.
 //
 Fittings.extend({
+  //
+  // Required name to identify the framework being pushed into Fittings.
+  //
+  name: 'reacts',
   fragment: read('arrive.html'),
+  bootstrap: read('bootstrap.html'),
   template: read('template.js'),
   plugin: read('plugin.js'),
   library: [
